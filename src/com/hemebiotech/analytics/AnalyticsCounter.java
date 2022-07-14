9772f7d680693c615c13e3631211a144571a656e
liticiakouam <108594256+liticiakouam@users.noreply.github.com>
2022-07-14 13:01:48 +0100

    Ecriture dans le fichier result.out (#3)

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,11 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
+	public static void main(String args[]) throws IOException {
 	//lecture des symptomes
 	ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 	List<String> symptoms = symptomReader.getSymptoms();
@@ -11,4 +13,9 @@ public class AnalyticsCounter {
 	//comptage du nombre d'occurrences de chaque symptome
 	ISymptomsCount counter = new SymptomsCounter();
 	Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
+
+	//ecriture dans le fichier de sortie
+	ISymptomWrite symptomWrite = new WriteSymptomDataToFile("result.out");
+	symptomWrite.writeToFile(symptomsWithOccurrences);
+	}
 }
--- src/com/hemebiotech/analytics/ISymptomReader.java
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -3,18 +3,22 @@ package com.hemebiotech.analytics;
 import java.util.List;
 
 /**
+ * ENGLISH
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
- * 
+ *
  * The implementation does not need to order the list
- * 
+ *
+ * FRANCAIS
+ * Je vous donne une interface pour vous permettre de lire un fichier dans une source
+ * Il retourne les resultats sous forme de chaine de caractère
  */
 public interface ISymptomReader {
-	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
-	 */
-	List<String> getSymptoms();
+    /**
+     * If no data is available, return an empty List
+     *
+     * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+     */
+    List<String> getSymptoms();
 }
