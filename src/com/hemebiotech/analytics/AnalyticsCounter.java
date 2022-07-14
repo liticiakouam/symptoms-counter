706a1d7582b8fc423875b67ae87bd0d4c757e1b0
liticiakouam <108594256+liticiakouam@users.noreply.github.com>
2022-07-14 11:39:01 +0100

    Comptage des occurences des symptomes dans la liste (#2)

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,10 +1,14 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	//lecture des symptomes
 	ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 	List<String> symptoms = symptomReader.getSymptoms();
 
+	//comptage du nombre d'occurrences de chaque symptome
+	ISymptomsCount counter = new SymptomsCounter();
+	Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
 }
